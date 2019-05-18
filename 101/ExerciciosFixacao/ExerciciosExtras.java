bd7fa1786446e461ab4473264f3ad9269dbbc851
Gustavo Delgado <gustavodelgado@protonmail.com>
2019-05-18 14:43:55 -0300

    Exercícios Extras

---
+++ 101/ExerciciosFixacao/ExerciciosExtras.java
@@ -0,0 +1,29 @@
+class ExerciciosExtras {
+    public static void main(String[] args) {
+        // EXERCICIO 1
+        // for (int contador = 150; contador <= 300; contador++) {
+        //     System.out.println(contador);
+        // }
+    // ===========================================
+        // EXERCICIO 2
+        // int soma = 0;
+        // for (int i = 1; i <= 1000; i++) {
+        //     soma += i;
+        //     System.out.println(soma);
+        // }
+    // ===========================================
+        // EXERCICIO 3
+        // for (int i = 1; i <= 100; i++) {
+        //     if (i%3 == 0) {
+        //         System.out.println(i);
+        //     }
+        // }
+     // ===========================================
+        // EXERCICIO 4
+        int fatorial = 1;
+        for (int n = 1; n <= 10; n++) {
+
+            System.out.println("O fatorial de " + n + " é: " + fatorial);
+        }
+    }
+}
