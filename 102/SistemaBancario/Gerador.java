ed41e0dc7244e212f2f63240ed1506687259ff5c
Gustavo Delgado <gustavodelgado@protonmail.com>
2019-05-25 17:00:11 -0300

    remoção da classe Gerador

--- 102/SistemaBancario/Gerador.java
+++
@@ -1,8 +0,0 @@
-class Gerador {
-    private int indiceDoGeradorDeNumeros;
-
-    public int numeros() {
-        this.indiceDoGeradorDeNumeros += 1;
-        return this.indiceDoGeradorDeNumeros;
-    }
-}
