6ce7ee3e2a26ab414a34f61683fdc70b62d08b3b
Gustavo Delgado <gustavodelgado@protonmail.com>
2019-06-15 13:19:56 -0300

    105 - Tratamento de exceptions

--- Projetos/sistemaBancario/src/br/com/caelum/contas/modelo/Conta.java
+++ Projetos/sistemaBancario/src/br/com/caelum/contas/modelo/Conta.java
@@ -42,7 +42,12 @@ public abstract class Conta {
     }
 
     public void deposita(double valor) {
-        this.saldo += valor;
+        if (valor < 0) {
+        	throw new IllegalArgumentException("Você tentou depositar "
+        									+ "um valor negativo!");
+        } else {
+        	this.saldo += valor;
+        }
     }
 
     public abstract String getTipo();
